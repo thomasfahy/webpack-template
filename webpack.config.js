b8b6b395dfc55176ae29d6844c419de0179d4e3d
thomasfahy <thomasfahy11@gmail.com>
2025-08-24 09:44:43 +0100

    optimized template for chunks and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
